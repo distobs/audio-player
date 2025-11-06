8b748b76afcc22c3c6cfc11d7993b8c26d2e4d39
distobs <cuppotatocake@gmail.com>
2025-11-06 16:53:39 -0300

    Start coding form submission.

--- script.js
+++ script.js
@@ -1,13 +1,35 @@
-let add_sample_button = document.querySelector(".add-sample-button");
-let add_sample_popup = document.querySelector(".popup-container");
-var audio_samples = [];
+const add_sample_button = document.querySelector(".add-sample-button");
+const add_sample_popup = document.querySelector(".popup-container");
+const close_popup = document.querySelector("#popup-closer")
+const file_form = document.querySelector("#form");
+let audio_samples = [];
 
-add_sample_popup.classList.add("hidden");
-
-add_sample_button.addEventListener('click', () => {
+function toggle_popup(add_sample_popup) {
 	if (add_sample_popup.classList.contains("hidden")) {
 		add_sample_popup.classList.remove("hidden");
 	} else {
 		add_sample_popup.classList.add("hidden");
+	}
+}
+
+toggle_popup(add_sample_popup); // start with closed popup
+
+add_sample_button.addEventListener('click', () => {
+	toggle_popup(add_sample_popup);
+})
+
+close_popup.addEventListener('click', () => {
+	toggle_popup(add_sample_popup);
+})
+
+file_form.addEventListener("submit", (event) => {
+	event.preventDefault();
+
+	const name = file_form.elements['name'].value;
+	const file = file_form.elements['file'].value;
+	
+	if (!name || !file) {
+		alert("Favor, preencher os campos.");
+	} else {
 	}
 })
