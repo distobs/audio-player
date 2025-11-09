94e5afe92a6982a64659db1d2bee12fbead0b903
distobs <cuppotatocake@gmail.com>
2025-11-08 23:19:52 -0300

    Implement adding sample buttons.

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
-const add_sample_button = document.querySelector(".add-sample-button");
+const add_sample_button = document.querySelector("#add-sample");
 const add_sample_popup = document.querySelector(".popup-container");
-const close_popup = document.querySelector("#popup-closer")
+const close_popup_button = document.querySelector("#popup-closer")
 const file_form = document.querySelector("#form");
-let audio_samples = [];
+let audio_samples = []
 
+/* SAMPLE LOGIC */
+function update_samples(audio_samples) {
+	const sample_grid = document.querySelectorAll(".sample-grid");
+	const sample_button = document.createElement("div");
+	const paragraph = document.createElement("p");
+	const sample_name = document.createTextNode(
+		audio_samples[audio_samples.length - 1]["name"]
+	);
+
+	paragraph.appendChild(sample_name);
+	sample_button.appendChild(paragraph);
+
+	sample_grid[0].insertBefore(sample_button, add_sample_button);
+
+	sample_button.classList.add("sample-button");
+}
+
+/* POPUP LOGIC */
 function toggle_popup(add_sample_popup) {
 	if (add_sample_popup.classList.contains("hidden")) {
 		add_sample_popup.classList.remove("hidden");
@@ -15,21 +33,34 @@ function toggle_popup(add_sample_popup) {
 toggle_popup(add_sample_popup); // start with closed popup
 
 add_sample_button.addEventListener('click', () => {
+		toggle_popup(add_sample_popup);
+})
+
+close_popup_button.addEventListener('click', () => {
 	toggle_popup(add_sample_popup);
 })
 
-close_popup.addEventListener('click', () => {
-	toggle_popup(add_sample_popup);
-})
-
+/* FORM LOGIC */
 file_form.addEventListener("submit", (event) => {
 	event.preventDefault();
 
-	const name = file_form.elements['name'].value;
-	const file = file_form.elements['file'].value;
-	
+	const name = file_form.elements['name'];
+	const file = file_form.elements['file'].files[0];
+
 	if (!name || !file) {
 		alert("Favor, preencher os campos.");
 	} else {
+		alert("Tudo O.K. com " + name.value + " e " + file.name);
+
+		audio_samples.push({
+			name: name.value,
+			file: file
+		});
+
+		update_samples(audio_samples);
+
+		file_form.reset();
+
+		toggle_popup(add_sample_popup);
 	}
 })
